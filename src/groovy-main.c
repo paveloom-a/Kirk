2c3c862450683455b1a9d7191686d0ceee5d3686
Pavel Sobolev <paveloom@riseup.net>
2023-06-26 11:59:26 +0300

    Copy basic "Hello, world!" program.

--- src/groovy-main.c
+++ src/groovy-main.c
@@ -14,6 +14,38 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-int main(int argc, char **argv) { printf("Hello there!\n"); }
+static void print_hello(GtkWidget *widget, gpointer data) {
+    g_print("Hello, world!\n");
+}
+
+static void activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window;
+    GtkWidget *button;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+
+    button = gtk_button_new_with_label("Hello, world!");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    gtk_window_set_child(GTK_WINDOW(window), button);
+
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new(
+        "paveloom.apps.groovy",
+        G_APPLICATION_DEFAULT_FLAGS
+    );
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
+}
